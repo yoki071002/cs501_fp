362dfd48840fa89b0c15611c7fa3584480478ead
Yutong (Yoki) Qin <yutongq@bu.edu>
2025-11-10 18:04:17 -0500

    gradle update on icon extension

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -73,5 +73,5 @@ dependencies {
     implementation("io.coil-kt:coil-compose:2.2.2")
     implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation("androidx.compose.material3:material3:1.2.0")
-    implementation("androidx.compose.material:material-icons-extended:1.5.0")
+    implementation("androidx.compose.material:material-icons-extended")
 }
